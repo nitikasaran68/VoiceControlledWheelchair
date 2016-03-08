3980e6a370c7e0f118d4a6f6fb340b9a096a1b2c
Ishita Verma <ishita.verma178@gmail.com>
2016-03-09 00:28:37 +0530

    First Commit

---
+++ H_Bridge_check.ino
@@ -0,0 +1,28 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,OUTPUT);  //M1 Enable -- Left Motor
+  pinMode(3,OUTPUT);  //M2 Enable -- Right Motor
+  pinMode(4,OUTPUT);  //M1 In 1
+  pinMode(5,OUTPUT);  //M1 In 2
+  pinMode(6,OUTPUT);  //M2 In 1
+  pinMode(7,OUTPUT);  //M2 In 2
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,LOW);
+    Serial.println("Yo");
+  
+  delay(2000);
+  digitalWrite(6,LOW);
+  digitalWrite(7,HIGH);
+  digitalWrite(4,LOW);
+  digitalWrite(5,HIGH);
+    Serial.println("Lo");
+
+  delay(2000); 
+}
